2407913e175a2adfb70b5fe6d0fc66a5ff59b1d6
jessygraceP <jacinth55jessy@gmail.com>
2023-11-29 16:45:13 +0530

    Navbar

---
+++ qtify/src/Components/Button/index.js
@@ -0,0 +1,5 @@
+import './Button.css'
+const Button = ({children}) => {
+    return <button className='button'>{children}</button>
+}
+export default Button;
---
+++ qtify/src/Components/Navbar/index.js
@@ -0,0 +1,14 @@
+
+import Button from "../Button"
+import Searchbox from "../Searchbox"
+import './Navbar.css'
+ const Navbar = () =>  {
+    return <div className="navbar">
+        <img src="Logo.PNG" alt="Logo" width={80} height={40} />
+        
+      
+        <Searchbox />
+        <Button>Give Feedback</Button>
+    </div>
+}
+export default Navbar;
